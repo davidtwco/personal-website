561b29bffd76f81171ae3359234c9fe52b9a1381
David Wood <david@davidtw.co>
2017-10-06 13:53:21 +0100

    Updated spacing.

--- scripts/app.js
+++ scripts/app.js
@@ -4,11 +4,13 @@ class TextScramble {
         this.chars = '!<>-_\\/[]{}—=+*^?#________'
         this.update = this.update.bind(this)
     }
+
     setText(newText) {
         const oldText = this.el.innerText
         const length = Math.max(oldText.length, newText.length)
         const promise = new Promise((resolve) => this.resolve = resolve)
         this.queue = []
+
         for (let i = 0; i < length; i++) {
             const from = oldText[i] || ''
             const to = newText[i] || ''
@@ -16,16 +18,20 @@ class TextScramble {
             const end = start + Math.floor(Math.random() * 40)
             this.queue.push({ from, to, start, end })
         }
+
         cancelAnimationFrame(this.frameRequest)
         this.frame = 0
         this.update()
         return promise
     }
+
     update() {
         let output = ''
         let complete = 0
+
         for (let i = 0, n = this.queue.length; i < n; i++) {
             let { from, to, start, end, char } = this.queue[i]
+
             if (this.frame >= end) {
                 complete++
                 output += to
@@ -39,6 +45,7 @@ class TextScramble {
                 output += from
             }
         }
+
         this.el.innerHTML = output
         if (complete === this.queue.length) {
             this.resolve()
@@ -47,6 +54,7 @@ class TextScramble {
             this.frame++
         }
     }
+
     randomChar() {
         return this.chars[Math.floor(Math.random() * this.chars.length)]
     }
