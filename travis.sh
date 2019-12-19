29dd02d2bc7004a0c551f6db91d3bb6bfcce2850
David Wood <david@davidtw.co>
2019-12-19 15:06:23 +0000

    Add another CTAN package that was split up.

--- travis.sh
+++ travis.sh
@@ -53,6 +53,7 @@ sudo /usr/local/texlive/2019/bin/x86_64-linux/tlmgr install latexmk \
   collection-fontsrecommended \
   pdftexcmds \
   letltxmacro \
+  pdfescape \
   ragged2e
 
 # Install Gulp.
